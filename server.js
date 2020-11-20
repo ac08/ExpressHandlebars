9bb03b8554895d44f99dd9a56ca453a062f6b4c7
Andrew Circelli <andrewcircelli@gmail.com>
2020-11-20 13:16:22 -0500

    begin front end javacript functionality burgers.js

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require("express");
+const path = require('path');
+const exphbs = require("express-handlebars");
+
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+// app.use(express.static("public"));
+
+// Sets up the Express app to establish a static directory to access static files
+app.use(express.static(path.join(__dirname, '/public')));
+// Serve the corresponding node_modules folder if file is not found in public
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
-const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
