39f249b727aaf94d2ce70dbe40692a4e16efe4ac
Andrew Circelli <andrewcircelli@gmail.com>
2020-11-22 13:57:17 -0500

    bootstrap styling on index.hbs file and update README.md

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,9 +7,9 @@ const burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required
 router.get("/", function(req, res) {
-  burger.all(function(data) {
+  burger.all(function(burgerData) {
     let hbsObject = {
-      burger: data
+      burger_data: burgerData
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
--- public/js/burgers.js
+++ public/js/burgers.js
@@ -1,25 +1,5 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".devoured").on("click", function(event) {
-    const id = $(this).data("id");
-
-    const newEatenState = {
-      devoured: 1
-    };
-
-    // Send the PUT request.
-    $.ajax("/api/burgers/" + id, {
-      type: "PUT",
-      data: newEatenState
-    }).then(
-      function() {
-        console.log("burger has been devoured");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
-
   $(".create-form").on("submit", function(event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
@@ -36,6 +16,26 @@ $(function() {
     }).then(
       function() {
         console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+  
+  $(".devoured").on("click", function(event) {
+    const id = $(this).data("id");
+
+    const newEatenState = {
+      devoured: 1
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newEatenState
+    }).then(
+      function() {
+        console.log("burger has been devoured");
         // Reload the page to get the updated list
         location.reload();
       }
